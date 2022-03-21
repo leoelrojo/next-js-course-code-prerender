11e1b2c266e1343139ffb296b9809a0964455372
wiel.leo <wiel.leo@gmail.com>
2022-03-21 18:14:35 +0100

    continuing prerender course

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,15 @@
 import path from 'path';
 import fs from 'fs/promises';
 
+import Link from 'next/link';
+
 function HomePage(props) {
   const { products } = props;
 
   return (
     <ul>
       {products.map((product) => (
-        <li key={product.id}>{product.title}</li>
+        <li key={product.id}><Link href={`/${product.id}`}>{product.title}</Link></li>
       ))}
     </ul>
   );
@@ -18,7 +20,7 @@ export async function getStaticProps() {
   const jsonData = await fs.readFile(filePath);
   const data = JSON.parse(jsonData);
 
-  return { props: { products: data.products } };
+  return { props: { products: data.products }, revalidate: 120 };
 }
 
 export default HomePage;
